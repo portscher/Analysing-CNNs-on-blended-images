634679067eeb5cc2b1965bf56fb67515ebea608b
andrea <andrea.portscher@student.uibk.ac.at>
2022-02-27 12:14:01 +0100

    remove "Visualize filters"

--- predict.py
+++ predict.py
@@ -75,51 +75,6 @@ elif args.arch.lower() == 'vision_transformer':
 arch = arch.get_model().to(device)
 
 ###################################################################################################################
-# Visualize and save filters
-###################################################################################################################
-model_weights = []  # we will save the conv layer weights in this list
-conv_layers = []  # we will save the 49 conv layers in this list
-
-# get all the model children as list
-model_children = list(arch.children())
-
-# counter to keep count of the conv layers
-counter = 0
-# append all the conv layers and their respective weights to the list
-# for i in range(len(model_children)):
-#     if type(model_children[i]) == nn.Conv2d:
-#         counter += 1
-#         model_weights.append(model_children[i].weight)
-#         conv_layers.append(model_children[i])
-#     elif type(model_children[i]) == nn.Sequential:
-#         for j in range(len(model_children[i])):
-#             for child in model_children[i][j].children():
-#                 if type(child) == nn.Conv2d:
-#                     counter += 1
-#                     model_weights.append(child.weight)
-#                     conv_layers.append(child)
-# print(f"Total convolutional layers: {counter}")
-#
-# # take a look at the conv layers and the respective weights
-# for weight, conv in zip(model_weights, conv_layers):
-#     print(len(conv_layers))
-#     print(f"CONV: {conv} ====> SHAPE: {weight.shape}")
-#
-# plt.figure(figsize=(20, 17))
-# for i, filter in enumerate(model_weights[args.layer]):
-#     plt.subplot(args.fsize, args.fsize, i + 1)
-#     plt.imshow(filter[0, :, :].detach().cpu().numpy(), cmap='viridis')
-#     plt.axis('off')
-#     plt.savefig(f'filters/{args.arch.lower()}_layer_{args.layer}.png')
-# plt.show()
-
-# Visualize conv filter
-
-kernels = arch.layer1[0].conv3.weight.detach().cpu()
-print(kernels.size())
-utils.visualize_tensor(kernels, args.arch.lower(), 3, ch=0, allkernels=False)
-
-###################################################################################################################
 # Start prediction and process results
 ###################################################################################################################
 
